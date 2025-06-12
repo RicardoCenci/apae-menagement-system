c1b4e644025db7318e3a111cc1f494f377542ecd
Ricardo Cenci Fabris <ricardocencifabris@gmail.com>
2025-06-11 21:20:16 -0300

    fix: authentication

--- routes/api.php
+++ routes/api.php
@@ -15,13 +15,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group([
-	'middleware' => 'api',
-	'prefix' => 'auth'
-], function () {
-	Route::post('login', [AuthController::class, 'login']);
-	Route::post('register', [AuthController::class, 'register']);
-	Route::post('logout', [AuthController::class, 'logout']);
-	Route::post('refresh', [AuthController::class, 'refresh']);
-	Route::get('me', [AuthController::class, 'me']);
-}); 
+Route::prefix('v1')->group(function () {
+
+	// Public routes
+	Route::group(['middleware' => 'auth:api'], function () {
+		Route::prefix('auth')->group(function () {
+			Route::post('login', [AuthController::class, 'login'])->name('login');
+			Route::post('register', [AuthController::class, 'register']);
+		});
+	});
+
+
+	// Protected routes
+	Route::group(['middleware' => 'auth:api'], function () {
+
+		Route::prefix('auth')->group(function () {
+			Route::post('logout', [AuthController::class, 'logout']);
+			Route::post('refresh', [AuthController::class, 'refresh']);
+			Route::get('me', [AuthController::class, 'me']);
+		});
+	});
+
+});
