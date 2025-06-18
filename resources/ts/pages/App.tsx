e5441b6f9ef8e2bd2b139a1d3d40e735cf25f8ca
Ricardo Cenci Fabris <ricardocencifabris@gmail.com>
2025-06-17 21:38:34 -0300

    feat: basic browser level routing

--- resources/ts/pages/App.tsx
+++ resources/ts/pages/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { HomePage } from './home';
+import { NotFoundPage } from './404';
 
 export function App() {
 	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
+		<BrowserRouter>
+			<div>
+				<div>
+					<h1>Menu</h1>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>	
+						</li>
+					</ul>
+				</div>
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="*" element={<NotFoundPage />} />
+					</Routes>
+			</div>
+		</BrowserRouter>
 	);
 }
---
+++ resources/ts/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '../../components/Button';
+
+export function HomePage() {
+	return (
+		<div>
+			<h1>Home Page</h1>
+			<Button>Click me</Button>
+		</div>
+	);
+}
